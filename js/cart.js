677ac6c8d477686104a0d6c48c2bf8625116817f
Hassel-Godspower <godspowerh0@gmail.com>
2026-01-01 09:52:17 -0600

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,21 @@
+const servicesDiv = document.getElementById("services");
+let cart = JSON.parse(localStorage.getItem("cart") || "[]");
 
+SPA_DATA.services.forEach(service => {
+  servicesDiv.innerHTML += `
+    <div class="card">
+      <h3>${service.name}</h3>
+      <p>$${service.price}</p>
+      <button class="btn" onclick="addToCart(${service.id})">
+        Add Service
+      </button>
+    </div>
+  `;
+});
+
+function addToCart(id) {
+  const service = SPA_DATA.services.find(s => s.id === id);
+  cart.push(service);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Service added to booking");
+}
