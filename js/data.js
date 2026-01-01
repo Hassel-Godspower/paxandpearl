d03f5e88bcb3da5305bc1bf86982b7de539dfb45
Hassel-Godspower <godspowerh0@gmail.com>
2026-01-01 09:23:37 -0600

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,10 @@
+const SPA_DATA = {
+  whatsappAdmin: "2348012345678", // admin WhatsApp number
 
+  services: [
+    { id: 1, name: "Deep Tissue Massage", price: 120 },
+    { id: 2, name: "Aromatherapy Massage", price: 100 },
+    { id: 3, name: "Facial Treatment", price: 80 },
+    { id: 4, name: "Luxury Spa Package", price: 200 }
+  ]
+};
