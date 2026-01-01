a9243b54831724c2f8dad1fe3e7e300fd54aebf0
Hassel-Godspower <godspowerh0@gmail.com>
2026-01-01 10:00:20 -0600

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,1 +1,26 @@
+function createCalendarEvent(name, services, date, time) {
+  const start = new Date(`${date}T${time}`);
+  const end = new Date(start.getTime() + 60 * 60 * 1000);
+
+  const ics =
+`BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VEVENT
+SUMMARY:Spa Booking - ${name}
+DESCRIPTION:${services}
+DTSTART:${formatICS(start)}
+DTEND:${formatICS(end)}
+END:VEVENT
+END:VCALENDAR`;
+
+  const blob = new Blob([ics], { type: 'text/calendar' });
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "spa-booking.ics";
+  link.click();
+}
+
+function formatICS(date) {
+  return date.toISOString().replace(/[-:]/g, '').split('.')[0];
+}
 
