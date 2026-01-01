8dabab64bc2876342709e8fc240ebf732de2b2fc
Hassel-Godspower <godspowerh0@gmail.com>
2026-01-01 10:03:41 -0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,13 @@
+const ADMIN = { user: "admin", pass: "1234" };
 
+function login() {
+  if (
+    user.value === ADMIN.user &&
+    pass.value === ADMIN.pass
+  ) {
+    localStorage.setItem("admin", "true");
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid credentials");
+  }
+}
